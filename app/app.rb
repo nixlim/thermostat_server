7f85adef2a27ea9a6d706e0177f3f6b1f0789788
Igor Ryabchuk <riabtchuk@gmail.com>
2018-05-18 10:59:25 +0100

    bear bones API workings

--- app/app.rb
+++ app/app.rb
@@ -1,13 +1,19 @@
 require 'sinatra/base'
+require 'json'
 
 
 class ThermoNuclearServer < Sinatra::Base
+  enable :sessions
 
   get '/' do
-    'hello world'
+    headers 'Access-Control-Allow-Origin' => '*'
+    @temp = {temp: 30, mode: 'on'}.to_json
+    haml :index
   end
 
   post '/thermonuclear' do
-    'hello world'
+    session[:powersaver] = 'on'
+    session[:temp] = 30
+    redirect '/'
   end
 end
